dda44a7376fb08fbdd773e439f3c972ca36f7dc4
Asadullah Khan <67080107+Khanmdasad@users.noreply.github.com>
2024-07-12 17:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//your JS code here. If required.
 function secondHighest(arr) {
+    // If the array length is less than 2, return -Infinity
     if (arr.length < 2) {
         return -Infinity;
     }
 
     let first = -Infinity;
     let second = -Infinity;
+    let uniqueValues = new Set();
 
     for (let i = 0; i < arr.length; i++) {
+        uniqueValues.add(arr[i]);
         if (arr[i] > first) {
             second = first;
             first = arr[i];
@@ -16,10 +18,10 @@ function secondHighest(arr) {
         }
     }
 
-    if (second === -Infinity) {
+    // If there is only one unique value in the array, return -Infinity
+    if (uniqueValues.size < 2) {
         return -Infinity;
     }
 
     return second;
 }
-.
